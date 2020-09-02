a7d34abbcb23505f28fbd6947e0cbf8480158079
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-01 23:29:44 -0300

    script about main page

---
+++ index.ts
@@ -0,0 +1,7 @@
+const view = window.document.querySelector('#push_view')
+const main_section = window.document.querySelector('#')
+const PushView = () => {
+
+}
+
+view.addEventListener('click', PushView)
