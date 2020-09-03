6f34c0f44bfeca3409baf10e9c9e718b82cfb332
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-02 23:54:01 -0300

    working in logic about main view

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-const view = window.document.querySelector('#push_view')
-const main_section = window.document.querySelector('#')
-const PushView = () => {
+const btnPush = window.document.querySelector('#btnPush')
+const mainSection = window.document.querySelector('#main-section')
+const mainFigure = window.document.querySelector('#main-figure')
+const viewContact = window.document.querySelector('#view-contact')
+const btnReturnBack = window.document.querySelector('#returnBack')
 
-}
+btnPush?.addEventListener('click', () => {
+  mainSection?.classList.remove()
+  mainSection?.classList.add('pushViewClass')
 
-view.addEventListener('click', PushView)
+  mainFigure?.classList.remove()
+  mainFigure?.classList.add('showOffFigureClass')
+
+  setTimeout(() => { viewContact.style.display = 'flex' }, 1000)
+})
+
+btnReturnBack?.addEventListener('click', () => {
+  viewContact.style.display = 'none'
+
+  mainSection?.className = ""
+  mainFigure?.className = ""
+})
