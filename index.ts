44c0cba8f6f5effde7b16fecc517c02b19f25e19
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-05 02:42:57 -0300

    working in animation of main page

--- index.ts
+++ index.ts
@@ -1,22 +1,32 @@
 const btnPush = window.document.querySelector('#btnPush')
+const mainContainer = window.document.querySelector('#MainContainer')
 const mainSection = window.document.querySelector('#main-section')
 const mainFigure = window.document.querySelector('#main-figure')
 const viewContact = window.document.querySelector('#view-contact')
 const btnReturnBack = window.document.querySelector('#returnBack')
+const navbar = window.document.querySelector('#navbar')
 
 btnPush?.addEventListener('click', () => {
+  if (window.innerHeight <= 767) {
+    mainContainer.style.marginTop = '0px'
+  }
   mainSection?.classList.remove()
   mainSection?.classList.add('pushViewClass')
 
+  navbar.style.display = 'none'
+
   mainFigure?.classList.remove()
   mainFigure?.classList.add('showOffFigureClass')
-
-  setTimeout(() => { viewContact.style.display = 'flex' }, 1000)
+  setTimeout(() => {
+    viewContact.style.display = 'flex'
+    viewContact.style.flexDirection = 'column'
+  }, 1000)
 })
 
 btnReturnBack?.addEventListener('click', () => {
   viewContact.style.display = 'none'
 
-  mainSection?.className = ""
-  mainFigure?.className = ""
+  mainSection?.className = ''
+  mainFigure?.className = ''
+  navbar.style.display = 'flex'
 })
