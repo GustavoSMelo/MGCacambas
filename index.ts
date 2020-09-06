ddbb6c71f1ae76345468d07a64f86815aab461bd
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-06 18:33:51 -0300

    script of display none menu mobile

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 const btnPush = window.document.querySelector('#btnPush')
-const mainContainer = window.document.querySelector('#MainContainer')
 const mainSection = window.document.querySelector('#main-section')
 const mainFigure = window.document.querySelector('#main-figure')
 const viewContact = window.document.querySelector('#view-contact')
@@ -7,9 +6,6 @@ const btnReturnBack = window.document.querySelector('#returnBack')
 const navbar = window.document.querySelector('#navbar')
 
 btnPush?.addEventListener('click', () => {
-  if (window.innerHeight <= 767) {
-    mainContainer.style.marginTop = '0px'
-  }
   mainSection?.classList.remove()
   mainSection?.classList.add('pushViewClass')
 
