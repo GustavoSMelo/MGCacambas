74903c8f55c03ff42f6c0fd0f0f7221752d69d69
Gustavo S. Melo <gsantos15569@gmail.com>
2020-10-15 20:53:01 -0300

    making changes of color and responsity, and add api to another system like email, facebook and instagram

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,11 @@
 var btnMenu = window.document.querySelector('#hamburgerMenu');
 var menu = window.document.querySelector('#menu-list');
+var wpp = window.document.querySelector('#wpp');
+var insta = window.document.querySelector('#insta');
+var email = window.document.querySelector('#email');
+var whatsapp = window.document.querySelector('#whatsapp');
+var instagram = window.document.querySelector('#instagram');
+var mail = window.document.querySelector('#mail');
 var onMenuDisplayed = false;
 var ShowMenu = function () {
     if (!onMenuDisplayed) {
@@ -17,5 +23,20 @@ var CloseMenu = function () {
         onMenuDisplayed = false;
     }
 };
+var SendMessageWpp = function () {
+    window.open('https://api.whatsapp.com/send?1=pt+br&phone=5511976708937');
+};
+var sendEmail = function () {
+    window.open('mailto:mg_cacambas@hotmail.com');
+};
+var openInsta = function () {
+    window.open('https://www.instagram.com/mg_cacambas/?igshid=rw1iub57i1as');
+};
 window.addEventListener('resize', CloseMenu);
 btnMenu === null || btnMenu === void 0 ? void 0 : btnMenu.addEventListener('click', ShowMenu);
+wpp === null || wpp === void 0 ? void 0 : wpp.addEventListener('click', SendMessageWpp);
+email === null || email === void 0 ? void 0 : email.addEventListener('click', sendEmail);
+insta === null || insta === void 0 ? void 0 : insta.addEventListener('click', openInsta);
+whatsapp === null || whatsapp === void 0 ? void 0 : whatsapp.addEventListener('click', SendMessageWpp);
+mail === null || mail === void 0 ? void 0 : mail.addEventListener('click', sendEmail);
+instagram === null || instagram === void 0 ? void 0 : instagram.addEventListener('click', openInsta);
--- src/ts/menu.ts
+++ src/ts/menu.ts
@@ -1,5 +1,12 @@
 const btnMenu = window.document.querySelector('#hamburgerMenu')
 const menu = window.document.querySelector('#menu-list')
+const wpp = window.document.querySelector('#wpp')
+const insta = window.document.querySelector('#insta')
+const email = window.document.querySelector('#email')
+const whatsapp = window.document.querySelector('#whatsapp')
+const instagram = window.document.querySelector('#instagram')
+const mail = window.document.querySelector('#mail')
+
 let onMenuDisplayed = false
 
 const ShowMenu = () => {
@@ -19,5 +26,24 @@ const CloseMenu = () => {
   }
 }
 
+const SendMessageWpp = () => {
+  window.open('https://api.whatsapp.com/send?1=pt+br&phone=5511976708937')
+}
+
+const sendEmail = () => {
+  window.open('mailto:mg_cacambas@hotmail.com')
+}
+
+const openInsta = () => {
+  window.open('https://www.instagram.com/mg_cacambas/?igshid=rw1iub57i1as')
+}
+
 window.addEventListener('resize', CloseMenu)
 btnMenu?.addEventListener('click', ShowMenu)
+wpp?.addEventListener('click', SendMessageWpp)
+email?.addEventListener('click', sendEmail)
+insta?.addEventListener('click', openInsta)
+whatsapp?.addEventListener('click', SendMessageWpp)
+mail?.addEventListener('click', sendEmail)
+instagram?.addEventListener('click', openInsta)
+
